1cc86cfceeef5034f52e17b0b270a42ec7c268a8
Mauro Estevez <mauroestevez20@outlook.es>
2022-07-14 17:39:51 -0300

    Modificamos el JS con las bases del futuro proyecto

--- js/index.js
+++ js/index.js
@@ -1,17 +1,38 @@
 
+
+// Armamos el creador de objeto para los clientes
+
+class Cliente {
+    constructor(nombre, edad) {
+        this.nombre = nombre.toUpperCase();
+        this.edad = edad;
+        this.asignado = false;
+    }
+    prestamoAsignado() {
+        this.asignado = true;
+    }
+};
+
+// Creamos los clientes ya existentes
+
+const mauro = new Cliente("Mauro", "21");
+const karina = new Cliente("Karina", "46");
+const franco = new Cliente("Franco", "18");
+const pablo = new Cliente("Pablo", "44");
+
 // Definimos una variable con el nombre del usuario
 
-let entrada = prompt("Ingrese su nombre por favor... (debe ser parte del grupo familiar)");
+let entrada = prompt("Ingrese su nombre por favor... (debe ser cliente del banco)");
 
-// Creamos un array con los miembros de la familia
+// Creamos un array con los clientes del banco
 
-const familia = ["karina", "mauro", "franco", "pablo", "choco"];
+const clientes = ["karina", "mauro", "franco", "pablo"];
 
-// Armamos un while para comprobar que el nombre del usuario coincida con alguna persona de la familia
+// Armamos un while para comprobar que el nombre del usuario coincida con algun cliente ya registrado del banco
 
-while(familia.includes(entrada) == false){
-    console.log("No eres un integrante de la familia");
-    entrada = prompt("Ingrese su nombre por favor... (debe ser parte del grupo familiar)");
+while(clientes.includes(entrada) == false){
+    console.log("No eres un cliente del banco");
+    entrada = prompt("Ingrese su nombre por favor... (debe ser cliente del banco)");
 }
 
 console.log("Bienvenid@ " + entrada + " !!");
@@ -19,7 +40,24 @@ console.log("Bienvenid@ " + entrada + " !!");
 
 // Asignamos las dos variables principales interactuando con el usuario
 
-let monto = parseInt(prompt("Ingrese el monto de la operación"));
+let monto = parseInt(prompt("Ingrese el monto que necesita:"));
+
+// Armamos una funcion para sumarle el interes cobrado por el banco al monto requerido
+
+function interes (num1) {
+    return num1 * 1.36;
+}
+
+// Creamos una variable para el monto final a pagar
+
+let montoTotal = interes(monto);
+
+// Le mostramos al cliente cual es el monto total
+
+console.log("La tasa de interés es del 36%, por lo que el monto total a abonar será de " + montoTotal + " pesos");
+
+// Le pedimos al cliente que elija la cantidad de cuotas
+
 let cantidadCuotas = parseInt(prompt("Ingrese cantidad de cuotas (3, 9 o 12)"));
 
 // Armamos un While para dar solo 3 opciones de cantidad de cuotas
@@ -32,6 +70,7 @@ while((cantidadCuotas != 3) && (cantidadCuotas != 9) && (cantidadCuotas != 12)){
 // Mostramos cuantas cuotas se han seleccionado
 
 console.log("Usted ha seleccionado " + cantidadCuotas + " cuotas");
+
 
 // Designamos una funcion para dividir
 
